de67e98dfe178e240b4b9daebbf4b544896a9b76
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-21 22:23:16 +0100

    timetamp

--- ProductivityTools.TrainingLog.Contract/Training.cs
+++ ProductivityTools.TrainingLog.Contract/Training.cs
@@ -4,12 +4,14 @@ namespace ProductivityTools.TrainingLog.Contract
 {
     public class Training
     {
+        public int TrainingId { get; set; }
         public string Name { get; set; }
+        public string Comment { get; set; }
         public string Sport { get; set; }
         public string Source { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
-        public TimeSpan Duration { get; set; }
+        public int Duration { get; set; }
         public float Distance { get; set; }
         public float Calories { get; set; }
         public float AverageSpeed { get; set; }
